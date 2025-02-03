277d6657f0cf931c20c38d8e34b39cfc99a225d0
Zoe Vaillant <63172002@edu.vaniercollege.qc.ca>
2025-02-03 16:18:29 -0500

    changesfeb3

--- public_html/web2/2dtransform_tutorial/js/2dtransform.js
+++ public_html/web2/2dtransform_tutorial/js/2dtransform.js
@@ -9,3 +9,16 @@ img3.addEventListener('click', function(event){
     //console.log(this.id)
     this.classList.toggle('move-right')
 })
+
+let infoSections = document.querySelectorAll('article section nav')
+
+infoSections.forEach(function (element) {
+   element.addEventListener('click', toggleInfo); 
+})
+
+
+function toggleInfo(event) {
+    console.log('toggleInfo');
+    this.parentNode.classList.toggle('visible')
+
+}
